4c062d9fa7c98e2a340b6cb86bd4ad6b57699271
Krystian Kat <krystian.kat@protonmail.com>
2022-04-01 00:45:46 +0200

    refactor: delete comments

--- socket/server.ts
+++ socket/server.ts
@@ -5,14 +5,10 @@ export function setupHandlers(io: Server) {
   io.on('connection', (socket) => {
     console.log('Client connected');
     socket.emit('connected', 'Connected');
+
     socket.on('Out message', (arg: string) => {
       console.log('Out message', arg);
-      // socket.broadcast.emit('In message', arg);
+      socket.broadcast.emit('In message', arg);
     });
-
-    // socket.on(OUT_MESSAGE, (arg) => {
-    //   console.log(arg);
-    //   socket.broadcast.emit(IN_MESSAGE, arg);
-    // });
   });
 }
