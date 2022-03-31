2752f40d2a4bad54cd6bb1115dcefe336b696135
Krystian Kat <krystian.kat@protonmail.com>
2022-03-31 08:11:42 +0200

    feat: add emit connected

--- socket/server.ts
+++ socket/server.ts
@@ -1,7 +1,14 @@
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
+import { IN_MESSAGE, OLD_MESSAGES, OUT_MESSAGE } from '../consts';
+// import { IMessage } from '../interfaces/message';
 
 export function setupHandlers(io: Server) {
-  io.on("connection", () => {
-    console.log("WOOHOO");
+  io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.emit('connected', 'Connected');
+    // socket.on(OUT_MESSAGE, (arg) => {
+    //   console.log(arg);
+    //   socket.broadcast.emit(IN_MESSAGE, arg);
+    // });
   });
 }
