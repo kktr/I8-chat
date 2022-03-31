201103c9619de500d7375d9c97f98f01f8ae8443
Krystian Kat <krystian.kat@protonmail.com>
2022-03-31 08:10:12 +0200

    refactor: lint, add intterfaces

--- server.ts
+++ server.ts
@@ -1,14 +1,33 @@
-import { createServer } from "http";
-const { parse } = require("url");
-const next = require("next");
-const { Server } = require("socket.io");
-const { setupHandlers } = require("./socket/server");
+import { createServer } from 'http';
+const { parse } = require('url');
+const next = require('next');
+const { Server } = require('socket.io');
+const { setupHandlers } = require('./socket/server');
 
-const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
 const port = 3000;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  hello: () => void;
+}
+
+interface InterServerEvents {
+  ping: () => void;
+}
+
+interface SocketData {
+  name: string;
+  age: number;
+}
 
 app.prepare().then(() => {
   const server = createServer(async (req, res) => {
@@ -17,12 +36,15 @@ app.prepare().then(() => {
 
       await handle(req, res, parsedUrl);
     } catch (err) {
-      console.error("Error occurred handling", req.url, err);
+      console.error('Error occurred handling', req.url, err);
       res.statusCode = 500;
-      res.end("internal server error");
+      res.end('internal server error');
     }
   });
+
   const io = new Server(server);
+  // const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server);
+
   setupHandlers(io);
   server.listen(port);
 });
