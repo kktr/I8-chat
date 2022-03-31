4db61e060eb639e8f6e40d9fedb94c124c517d48
Krystian Kat <krystian.kat@protonmail.com>
2022-03-31 08:11:21 +0200

    feat: create Message component

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack } from '@mui/material';
 
 type MessageProps = {
   text: string;
@@ -6,16 +6,16 @@ type MessageProps = {
 
 export const Message = ({ text }: MessageProps) => {
   return (
-    <Box sx={{ marginBottom: "5px" }}>
+    <Box sx={{ marginBottom: '5px' }}>
       <Stack
         direction="row"
         sx={{
-          display: "inline-flex",
-          padding: "10px 10px",
+          display: 'inline-flex',
+          padding: '10px 10px',
           borderRadius: 2,
           border: 1,
-          borderColor: "rgba(140, 201, 247, 0.8)",
-          backgroundColor: "rgb(225, 241, 253, 0.8)",
+          borderColor: 'rgba(140, 201, 247, 0.8)',
+          backgroundColor: 'rgb(225, 241, 253, 0.8)',
         }}
       >
         {text}
